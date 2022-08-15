bf1bac81fbe600e0b3b522b071586388c9241bfa
sweetundertaker <nikitalutsenko030796@yandex.ru>
2022-08-15 15:14:06 +0300

    hello

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
-  $box=[];
+/*  $box=[];
 $box[1]="Первая коробка содержит рыбу";
-$box[2]="Вторая коробка содержит фрукты";
+$box['joys']="Вторая коробка содержит игрушки";
 
-echo $box[1];
+echo $box[1] . PHP_EOL;
+echo $box['joys']. PHP_EOL;
+*/
+
+class Human {
+    public $name;
+
+    public $age;
+
+    public function setName($name) {  // для чего мы указали в скобочках $name
+        $this->name = $name; //  для чего ипользуюется $this
+    }
+}
+
+//$var - Объект, экземпляр класса Human
+$var = new Human();
