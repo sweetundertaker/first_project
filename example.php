b12601750bc57c7678208f972d8a6b77119e8a1c
sweetundertaker <nikitalutsenko030796@yandex.ru>
2022-11-09 15:46:27 +0300

    abracadabra

--- example.php
+++ example.php
@@ -1,5 +1,13 @@
 <?php
 //task1
-$word = 'julius';
-$key = 'aaa';
+$word = "julius";
+$key = 3;
+for($i=0; $i<strlen($word); $i++){
+    $symbol = ord($word[$i])+$key;
+    if ($symbol > 255){
+        $symbol = $symbol - 255;
+    }
+    $codeWord = $codeWord .chr($symbol);
+}
+echo  $codeWord;
 
