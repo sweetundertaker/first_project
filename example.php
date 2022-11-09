8800a4afe8ee1990ddcf221221c96f7df214d895
sweetundertaker <nikitalutsenko030796@yandex.ru>
2022-11-09 16:43:46 +0300

    daw

--- example.php
+++ example.php
@@ -1,13 +1,26 @@
 <?php
 //task1
+
+
 $word = "julius";
 $key = 3;
-for($i=0; $i<strlen($word); $i++){
-    $symbol = ord($word[$i])+$key;
-    if ($symbol > 255){
+$codeWord = ' ';
+$discodeWord = ' ';
+for ($i = 0; $i < strlen($word); $i++) {
+    $symbol = ord($word[$i]) + $key;
+    if ($symbol > 255) {
         $symbol = $symbol - 255;
     }
-    $codeWord = $codeWord .chr($symbol);
+    $codeWord = $codeWord . chr($symbol);
 }
-echo  $codeWord;
+echo $codeWord;
+
+for ($i = 0; $i < strlen($codeWord); $i++) {
+    $symbol = ord($codeWord[$i]) - $key;
+    if ($symbol < 255) {
+        $symbol = 255 - $symbol;
+    }
+    $discodeWord = $discodeWord . chr($symbol);
+}
+echo $discodeWord;
 
