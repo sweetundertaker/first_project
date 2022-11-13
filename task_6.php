55f10223eabb415331f00eb1fe3ade5163b247c0
sweetundertaker <nikitalutsenko030796@yandex.ru>
2022-11-13 10:39:26 +0300

    daw

--- task_6.php
+++
@@ -1,5 +0,0 @@
-<?php
-$number = 10;
-$nullable = null;
-$result = $nullable - $number;
-echo $result;
---
+++ task_7.php
@@ -0,0 +1,8 @@
+<?php
+$firstNumber = 10;
+$secondNumber = 2;
+$sum = $firstNumber + $secondNumber;
+$subtraction = $firstNumber - $secondNumber;
+$multiplication = $firstNumber * $secondNumber;
+$division = $firstNumber / $secondNumber;
+echo $secondNumber;
